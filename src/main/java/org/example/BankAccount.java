cbb91d1a47b01ce26e412a1bb8e9fc674899e4c7
manbarmohamed <you@example.com>
2024-06-24 18:23:47 +0100

    task 4 checkBalance

---
+++ src/main/java/org/example/BankAccount.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class BankAccount {
+
+
+    public int checkBalance(int deposit, int withdrawal) {
+        return deposit - withdrawal;
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("enter your deposit amount: ");
+        Scanner sc = new Scanner(System.in);
+        int depositAmount = sc.nextInt();
+        System.out.println("enter your withdrawal amount: ");
+        int withdrawalAmount = sc.nextInt();
+        BankAccount bankAccount = new BankAccount();
+        System.out.println("Balance: "+ bankAccount.checkBalance(depositAmount,withdrawalAmount));
     }
 }
